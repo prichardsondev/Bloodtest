c524259086c4400783588dbf095a37e9921e424f
prichardsonDev <prichardsonDev@gmail.com>
2019-05-14 21:59:11 -0400

    update port to 80

---
+++ server.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+require("./middleware/startup")(app);
+require("./routes/startup")(app);
+require("./database/db")();
+
+let port = 80;
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
