4076bf6638bb660ca3cdb505001c912f1ed77a06
prichardsonDev <prichardsonDev@gmail.com>
2019-05-23 05:10:40 -0400

    port 80

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("./middleware/startup")(app);
 require("./routes/startup")(app);
 require("./database/db")();
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 80;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
